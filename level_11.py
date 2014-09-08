17f583f07778182127868777fa46bfa6678d3cef
jhpx <jhpx@hotmail.com>
2014-09-08 19:51:46 +0800

    Add level_11.py.

---
+++ level_11.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+#!/bin/env python
+# Odd even of an image.
+import urllib
+import re
+import Image  # need PIL 1.1.7+
+
+PREFIX = "http://huge:file@www.pythonchallenge.com/pc/return/"
+url = PREFIX + 'cave.jpg'
+
+
+def catch(url, cnt=0):
+    return urllib.urlretrieve(url)[cnt]
+
+
+def solve(fname):
+    im = Image.open(fname)
+    data = list(im.getdata())
+    im.putdata(data[::2] + data[1::2])
+    im.show()
+
+
+if __name__ == "__main__":
+    text = catch(url)
+    answer = solve(text)
+    # evil
+
+    # url: http://www.pythonchallenge.com/pc/return/evil.html
