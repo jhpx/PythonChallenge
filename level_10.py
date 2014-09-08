5ba897a2e3ca2c24039f313e6d73d66814cbb95c
jhpx <jhpx@hotmail.com>
2014-09-08 17:04:00 +0800

    Add level_10.py.

---
+++ level_10.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+#!/bin/env python
+import re
+
+PREFIX = "http://www.pythonchallenge.com/pc/return/"
+url = PREFIX + 'bull.html'
+
+
+def describe(x):
+    return "".join([str(len(i + j)) + i for i, j in re.findall(r"(\d)(\1*)", x)])
+
+
+def solve(n):
+    x = "1"
+    for each in range(n):
+        x = describe(x)
+    return str(len(x))
+
+if __name__ == "__main__":
+    answer = solve(30)
+    # 5808
+    print PREFIX + answer + '.html'
+    # url: http://www.pythonchallenge.com/pc/return/5808.html
