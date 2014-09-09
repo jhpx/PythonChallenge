66408fa5b0de9a59f4899477aafed9850b7b0382
jhpx <jhpx@hotmail.com>
2014-09-09 21:01:36 +0800

    Add level_14.py.

---
+++ level_14.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+#!/bin/env python
+# 100*100 = (100+99+99+98) + (...
+import urllib
+# never use PIL 1.1.7 but Pillow 2.5+
+from PIL import Image
+
+
+PREFIX = "http://huge:file@www.pythonchallenge.com/pc/return/"
+url = PREFIX + 'wire.png'
+
+
+def catch(url, cnt=0):
+    return urllib.urlretrieve(url)[cnt]
+
+
+def solve(fname):
+    unknown = Image.new('RGB', (100, 100))
+    im = Image.open(fname)
+    piv = unknown.load()
+    i, j = 0, 0
+    L = 99   # length of sprial edge, decrease by 2 circle by circle
+    N = 0    # count accessed pixels, increase by 4*L circle by circle
+    for k, px in enumerate(im.getdata()):
+        piv[i, j] = px
+        if (k - N) // L == 0:   # right
+            i += 1
+        elif (k - N) // L == 1:  # down
+            j += 1
+        elif (k - N) // L == 2:  # left
+            i -= 1
+        elif (k - N + 1) // L == 3:  # up
+            j -= 1
+        elif (k - N + 1) // L == 4:  # nex circle
+            i += 1
+            N += 4 * L
+            L -= 2
+    unknown.show()
+
+
+if __name__ == "__main__":
+    text = catch(url)
+    answer = solve(text)
+    # cat
+
+    # url: http://huge:file@www.pythonchallenge.com/pc/return/cat.html
