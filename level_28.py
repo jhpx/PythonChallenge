86bb7a4bc490b2eb676e23abbc022e8290491a00
jhpx <jhpx@hotmail.com>
2017-02-08 13:12:12 +0800

    Add level_28.py

---
+++ level_28.py
@@ -0,0 +1,41 @@
+#!/bin/env python
+# coding=utf-8
+# http://repeat:switch@www.pythonchallenge.com/pc/ring/bell.html
+# Difference between next(green) and next(green).
+import requests
+from io import BytesIO
+from PIL import Image
+
+PREFIX = "http://repeat:switch@www.pythonchallenge.com/pc/ring/"
+url = PREFIX + 'bell.png'
+
+
+def paired(data):
+    data = iter(data)
+    while True:
+        yield next(data), next(data)
+
+
+def solve(something):
+    im = Image.open(BytesIO(something))
+    red, green, blue = im.split()
+    result = []
+    for a, b in paired(green.getdata()):
+        diff = abs(a - b)
+        if diff != 42:
+            result.append(diff)
+    answer = bytes(result).decode()
+    return answer
+
+
+if __name__ == "__main__":
+    r = requests.get(url)
+    something = r.content
+    answer = solve(something)
+
+    print(answer)
+    # whodunnit().split()[0] ?
+
+    # Python was developed by Guido van Rossum
+
+    # http://repeat:switch@www.pythonchallenge.com/pc/ring/guido.html
